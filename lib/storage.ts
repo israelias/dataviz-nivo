65151fc19d3f0fa65797a05a6ab68a8db6a1c796
israelias <israelias.js@gmail.com>
2021-09-07 10:07:35 +0800

    Add localstorage hook

---
+++ lib/storage.ts
@@ -0,0 +1,17 @@
+export const storage = {
+  getToken: () =>
+    JSON.parse(window.localStorage.getItem('token') || '{}'),
+  setToken: (token: string) =>
+    window.localStorage.setItem('token', JSON.stringify(token)),
+  clearToken: () => window.localStorage.removeItem('token'),
+  setUserLocal: (username: string) =>
+    localStorage.setItem('username', username),
+  getUserLocal: () =>
+    JSON.parse(window.localStorage.getItem('username') || ''),
+  clearUserLocal: () => window.localStorage.removeItem('username'),
+  setLogoutEvent: () =>
+    window.localStorage.setItem(
+      'app_logout',
+      JSON.stringify(Date.now())
+    ),
+};
