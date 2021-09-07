f61a95c4f96ecfc61b28b91364a0762194536370
israelias <israelias.js@gmail.com>
2021-09-07 15:15:12 +0800

    Add custom error page

---
+++ pages/_error.tsx
@@ -0,0 +1,65 @@
+import {
+  Container,
+  Icon,
+  Stack,
+  Flex,
+  Heading,
+  Text,
+  Button,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+
+const Error = ({ statusCode }) => {
+  return (
+    <Flex align={'center'} justify={'center'} h={'100vh'} w={'full'}>
+      <Stack
+        as={Container}
+        bg={'#1c1d1f'}
+        rounded={'xl'}
+        p={8}
+        spacing={6}
+        maxW={'lg'}
+        align={'center'}
+        textAlign={'center'}
+      >
+        {/* <Icon as={Logo} w={10} h={10} /> */}
+        <Stack spacing={2}>
+          <Heading>
+            {statusCode ? statusCode.toString() : 'Page not found'}
+          </Heading>
+          <Text>
+            {statusCode
+              ? `An error ${statusCode} occurred on server`
+              : 'An error occurred on client'}
+          </Text>
+        </Stack>
+        <Flex>
+          <NextLink href={'/'} passHref>
+            <Button
+              as={'a'}
+              rounded={'full'}
+              bg={'#f1c857'}
+              color={'white'}
+              _hover={{
+                bg: '#3f414b',
+              }}
+            >
+              Take me to the home page
+            </Button>
+          </NextLink>
+        </Flex>
+      </Stack>
+    </Flex>
+  );
+};
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res
+    ? res.statusCode
+    : err
+    ? err.statusCode
+    : 404;
+  return { statusCode };
+};
+
+export default Error;
