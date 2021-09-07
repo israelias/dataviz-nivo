46f2cd50ca42e424091536c48ea295f1a4f1f731
israelias <israelias.js@gmail.com>
2021-09-07 10:13:12 +0800

    Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,54 @@
+import {
+  Container,
+  Icon,
+  Stack,
+  Flex,
+  Heading,
+  Text,
+  Button,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+
+export const NotFound = () => {
+  return (
+    <Flex align={'center'} justify={'center'} h={'100vh'} w={'full'}>
+      <Stack
+        as={Container}
+        bg={'#1c1d1f'}
+        rounded={'xl'}
+        p={8}
+        spacing={6}
+        maxW={'lg'}
+        align={'center'}
+        textAlign={'center'}
+      >
+        {/* <Icon as={Logo} w={10} h={10} /> */}
+        <Stack spacing={2}>
+          <Heading>Page not found</Heading>
+          <Text>
+            This page was not found. You may have mistyped the address
+            or the page may have moved as this project is currently in
+            development.
+          </Text>
+        </Stack>
+        <Flex>
+          <NextLink href={'/'} passHref>
+            <Button
+              as={'a'}
+              rounded={'full'}
+              bg={'#f1c857'}
+              color={'white'}
+              _hover={{
+                bg: '#3f414b',
+              }}
+            >
+              Take me to the home page
+            </Button>
+          </NextLink>
+        </Flex>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default NotFound;
