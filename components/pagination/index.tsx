edb3e0407d0179b6e48c37cfdfe4b94f6976bf28
israelias <israelias.js@gmail.com>
2021-09-08 15:08:33 +0800

    Add in-progress pagination

---
+++ components/pagination/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  Flex,
+  HStack,
+  Button,
+  useColorModeValue as mode,
+} from '@chakra-ui/react';
+
+const Pagination = ({
+  hasPrev,
+  hasNext,
+  page,
+  setPage,
+}: {
+  hasPrev: boolean | undefined;
+  hasNext: boolean | undefined;
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+}) => (
+  <>
+    <Flex
+      id="pagination"
+      height="62px"
+      top={{ base: '39px', lg: 0 }}
+      zIndex={10}
+      position="sticky"
+      width="100%"
+      maxWidth={{ base: '100vw' }}
+      bg={mode('#fff', '#141625')}
+    >
+      <Flex
+        alignItems="center"
+        justifyContent="space-between"
+        width="100%"
+        pl="10px"
+        pr="10px"
+      >
+        <HStack
+          display="flex"
+          spacing={8}
+          alignItems="center"
+          width="100%"
+          justifyContent="space-between"
+        >
+          <Button
+            disabled={!hasPrev}
+            onClick={() => setPage(page - 1)}
+          >
+            Previous
+          </Button>
+          <Button
+            disabled={!hasNext}
+            onClick={() => setPage(page + 1)}
+          >
+            Next
+          </Button>
+        </HStack>
+      </Flex>
+    </Flex>
+  </>
+);
+
+export default Pagination;
