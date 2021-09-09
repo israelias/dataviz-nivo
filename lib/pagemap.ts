4f1adcccec3fddb176c26692fe81e79488efad4c
israelias <israelias.js@gmail.com>
2021-09-10 02:23:39 +0800

    Add comments and purge unused in lib

--- lib/pagemap.ts
+++ lib/pagemap.ts
@@ -1,34 +1,36 @@
 import { range } from '../utils/array';
 
+//TODO Option to getStaticProps and attach page props to segments of static, pre-fetched pokemons
+
 export default [
   {
     x: 1,
     first: 25,
-    limit: range(1, 26),
+    limit: range(0, 26),
   },
   {
     x: 2,
     first: 50,
-    limit: range(26, 51),
+    limit: range(25, 51),
   },
   {
     x: 3,
     first: 75,
-    limit: range(51, 76),
+    limit: range(50, 76),
   },
   {
     x: 4,
     first: 100,
-    limit: range(76, 101),
+    limit: range(75, 101),
   },
   {
     x: 5,
     first: 125,
-    limit: range(101, 126),
+    limit: range(100, 126),
   },
   {
     x: 6,
     first: 150,
-    limit: range(126, 151),
+    limit: range(125, 151),
   },
 ];
