0cf308835364fa2e5b7da52d12cc1bb007fa91b0
Joem Elias Sanez <israelias.js@gmail.com>
2023-01-02 22:05:54 +0800

    Lock nextjs version to 12

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ module.exports = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
   async redirects() {
     return [
       {
